b3309f6cf1bc74c7dfd4a2c3b64c063aea93af1d
Santiago <tobosan@hotmail.com>
2015-05-07 14:55:44 -0500

    ultima

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -1,2 +1,3 @@
 class Participante < ActiveRecord::Base
+	validates_presence_of :cc, :primer_Nombre, :segundo_Nombre, :primer_Apellido, :segundo_Apellido, :eps, :ciudad, :telefono, :categoria, :edad
 end
